c892362f3240003e9915212d4078b5a473a87fc2
pc05 <pc05@DESKTOP-HPHF7D3>
2017-04-21 11:18:52 -0500

    mainJar

--- proyectojar/src/proyectojar/mainjar.java
+++ proyectojar/src/proyectojar/mainjar.java
@@ -1,10 +1,12 @@
 package proyectojar;
 
+import jvoz.JVozApp;
+
 public class mainjar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JVozApp.main(new String[]{""});
 	}
 
 }
